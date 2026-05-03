17c4fcc45c5047a050a6641e6ff86995e19fddd7
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2026-05-03 22:43:43 +0200

    Add profile related behaviours: next, prev, select

---
+++ app/include/dt-bindings/zmk/profile.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define PROFILE_NXT_CMD 0
+#define PROFILE_PRV_CMD 1
+#define PROFILE_SEL_CMD 2
+
+/*
+Note: Some future commands will include additional parameters, so we
+defines these aliases up front.
+*/
+
+#define PROFILE_NXT PROFILE_NXT_CMD 0
+#define PROFILE_PRV PROFILE_PRV_CMD 0
+#define PROFILE_SEL PROFILE_SEL_CMD
